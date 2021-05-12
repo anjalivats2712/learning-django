d7e63a1d0d1e982fe757c12ca392ee8f9f41d79c
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-05-12 14:05:14 +0530

    commit

--- hello/newapp/urls.py
+++ hello/newapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from newapp import views
 
 urlpatterns = [
-    path('',views.index,name="newapp")
+    path('',views.index,name="home"),
+    path('about',views.about,name="about"),
+    path('services',views.services,name="services"),
+    path('contact',views.contact,name="contact"),
+
 ]
--- hello/newapp/views.py
+++ hello/newapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request,"index.htm")
+    #return HttpResponse("this is the homepage")
+def about(request):
+    return HttpResponse("this is the aboutpage")
+def contact(request):
+    return HttpResponse("this is the contactpage")
+def services(request):
+    return HttpResponse("this is the servicespage")
