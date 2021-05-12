7711220cd2089762b1eb6d3d0cf0df56c97b4ce0
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-05-12 11:41:29 +0530

    commit

--- hello/newapp/urls.py
+++ hello/newapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from home import views
+from newapp import views
 
 urlpatterns = [
-    path('',views.index,name="home")
+    path('',views.index,name="newapp")
 ]
